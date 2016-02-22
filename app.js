e619b95b68dcfb521cc32bbdbb1d4e1f0065b129
gvp-hemanthreddy <hemanthreddy519@gmail.com>
2016-02-22 12:50:21 +0530

    Change in node port listening logic.

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
 
-app.listen(3000, function() {
-	console.log('Listening on port: 3000');
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
